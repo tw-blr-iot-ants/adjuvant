b3c2402f0985324e0af72c929f088944dcedc590
Renjith G R <grenjithr@gmail.com>
2015-09-23 02:29:34 +0530

    Dealing with the file changes made in the previous commit

--- admin/app/routes.js
+++ admin/app/routes.js
@@ -1,30 +1,8 @@
-var Menu = require('./models/menuDB')
-var Register = require('./models/registerDB')
-var Beverage = require('./models/juice.js');
-
-
-var getDetails= function(res){
-	Menu.find(function(err, menu) {
-			if (err)
-				res.send(err)
-			res.json(menu);
-		});
-};
+var Register = require('./models/registerDB');
+var Beverage = require('./models/beverage.js');
 
 module.exports = function(app) {
-
-	app.post('/api/setupJuices', function(req, res) {
-	  var conditions = {};
-	  conditions.Juice = req.body.Juice;
-	  var update = {};
-	  update.Cost =  req.body.Cost;
-	  return Menu.update(conditions, update, {"upsert": true}, function(error, menu) {
-	                if(error)
-	                    res.send(err);
-	              	getDetails(res);
-	  })
-	})
-
+	
 	app.post('/api/getInvoice', function(req, res) {
 	   return Register.find({"Date" : req.body.Date}, function(error, register) {
 	                if(error)
@@ -33,20 +11,6 @@ module.exports = function(app) {
 	  })
 	})
 
-	app.post("/api/deleteJuice", function(req, res) {
-	    console.log("req.body.Juice", req.body);
-	    return Menu.remove({"Juice": req.body.juiceToBeDeleted}, function(error, menu) {
-	        if (error)
-        		res.send(error);
-
-        	getDetails(res);
-	    })
-	})
-
-	app.get('/api/getJuices', function(req, res) {
-	  getDetails(res);
-	})
-	
 	app.post('/api/beverages/', function(req, res) {
 		console.log(req.body.name);
 		
