df7b2a4b6e3e386346d1c15db001820638fb36b1
Renjith G R <grenjithr@gmail.com>
2015-09-24 17:25:51 +0530

    Correcting the path to the public folder

--- admin/app/server.js
+++ admin/app/server.js
@@ -4,7 +4,7 @@ var port  	 = process.env.PORT || 8083;
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/../public/'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
