1b5f39efe27f471da3155afa10d0cdd6adf994e9
bndeepti <bndeepti@gmail.com>
2015-09-25 12:25:28 +0530

    Deepti - Flushing order records after 6 months by default

--- admin/app/models/order.js
+++ admin/app/models/order.js
@@ -1,11 +1,15 @@
 var mongoose = require("mongoose");
+var moment = require("moment");
 
 var orderSchema = new mongoose.Schema({
   Date: { type: Date },
   Name: { type: String },
   EmployeeId: String,
   DrinkName: String,
-  Quantity: { type: Number }
+  Quantity: { type: Number },
+  expiresAt: { type: Date, default: moment(moment()+moment.duration(6, 'months'))}
 });
+
+orderSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 })
 
 module.exports = mongoose.model('Order', orderSchema);
