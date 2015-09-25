1799078eb498796959835a11ae2a40de7d8e6d43
bndeepti <bndeepti@gmail.com>
2015-09-25 21:09:57 +0530

    Revert "Deepti - Flushing order records after 6 months by default"
    This reverts commit 1b5f39efe27f471da3155afa10d0cdd6adf994e9.

--- admin/app/models/order.js
+++ admin/app/models/order.js
@@ -1,15 +1,11 @@
 var mongoose = require("mongoose");
-var moment = require("moment");
 
 var orderSchema = new mongoose.Schema({
   Date: { type: Date },
   Name: { type: String },
   EmployeeId: String,
   DrinkName: String,
-  Quantity: { type: Number },
-  expiresAt: { type: Date, default: moment(moment()+moment.duration(6, 'months'))}
+  Quantity: { type: Number }
 });
-
-orderSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 })
 
 module.exports = mongoose.model('Order', orderSchema);
