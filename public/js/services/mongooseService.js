7c710c251a0db7ca22d976fd6f3adca86967098e
dixith <dixxit_k@ymail.com>
2015-09-27 11:48:22 +0530

    Dixith | Deleted Client facing webapp

--- public/js/services/mongooseService.js
+++
@@ -1,9 +0,0 @@
-angular.module("mongooseService", [])
-    .factory('mongooseService', ['$http', function($http) {
-
-      return {
-        update : function(data) {
-            return $http.post("/api/updateDB", data)
-        }
-      }
-}]);
