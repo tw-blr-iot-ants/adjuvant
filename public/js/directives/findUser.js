73337a09369cd8324898df5e03b76ef8f0d5fc55
bndeepti <bndeepti@gmail.com>
2016-01-17 17:20:41 +0530

    Deepti | Integrate find, update and delete user

--- public/js/directives/findUser.js
+++ public/js/directives/findUser.js
@@ -8,23 +8,84 @@ adjuvant.directive('findUser', ['mongooseService', '$timeout',
         scope: {},
         link: function(scope) {
 
-            scope.showSuccessAlert = false;
             scope.employeeId = "";
+            scope.employeeName = "";
             scope.result = "";
+            scope.shouldDisplayResult = true;
 
             scope.findUser = function() {
                       mongooseService.findUser({"empId": scope.employeeId})
                                           .then(function(response){
                                               scope.result = response.data;
+                                              scope.employeeName = response.data.employeeName;
                                           }, _notifyError)
             }
 
             var _notifyError = function(response) {
-                  scope.showErrorAlert = true;
+                  scope.shouldDisplayResult = false;
+                  scope.showFindErrorAlert = true;
                   $timeout(function() {
-                    scope.showErrorAlert = false;
-                  }, 1000)
+                    _resetDefaults();
+                  }, 3000)
             }
+
+            scope.deleteUser = function() {
+                  mongooseService.deleteUser({"empId": scope.employeeId})
+                                           .then(_notifyDeleteSuccess, _notifyDeleteError)
+            }
+
+            var _notifyDeleteSuccess = function(response) {
+                  scope.shouldDisplayResult = false;
+                  scope.showDeleteSuccessAlert = true;
+                  $timeout(function() {
+                      _resetDefaults();
+                  }, 3000)
+            }
+
+            var _notifyDeleteError = function(response) {
+                  scope.showDeleteErrorAlert = true;
+                  $timeout(function() {
+                    _resetDefaults();
+                  }, 3000)
+            }
+
+            scope.updateUser = function(employeeName) {
+                 var user = _constructUser(employeeName);
+                 mongooseService.updateUser(user)
+                                     .then(_notifyUpdateSuccess, _notifyUpdateError)
+            }
+
+            var _notifyUpdateSuccess = function(response) {
+                   scope.showUpdateSuccessAlert = true;
+                   $timeout(function() {
+                        _resetDefaults();
+                   }, 3000)
+            }
+
+            var _notifyUpdateError = function(response) {
+                  scope.showUpdateErrorAlert = true;
+                  $timeout(function() {
+                    _resetDefaults();
+                  }, 3000)
+            }
+
+             var _constructUser = function(employeeName) {
+                  return {empId: scope.result.empId,
+                          employeeName:  employeeName,
+                          internalNumber:  scope.result.internalNumber};
+             }
+
+            var _resetDefaults = function() {
+                  scope.employeeId = "";
+                  scope.result = "";
+                  scope.showFindErrorAlert = false;
+                  scope.shouldDisplayResult = true;
+                  scope.showDeleteSuccessAlert = false;
+                  scope.showDeleteErrorAlert = false;
+                  scope.showUpdateSuccessAlert = false;
+                  scope.showUpdateErrorAlert = false;
+            }
+            _resetDefaults();
         }
     }
 }]);
--- public/test/unit/directives/findUser.spec.js
+++ public/test/unit/directives/findUser.spec.js
@@ -1,10 +1,17 @@
-describe('findUser', function() {
-    var element, mockMongooseService, scope, mongooseServicePromise
+describe.only('findUser', function() {
+    var element, mockMongooseService, scope, mongooseServicePromise, employee, updatedEmployee;
 
 
     beforeEach(function() {
-       mongooseServicePromise = Q({data: ["dummy"]})
-       mockMongooseService = { findUser: sinon.stub().returns(mongooseServicePromise) };
+       employee = {employeeId: "123", employeeName: "Ravi", internalNumber: "000"};
+       mongooseServicePromise = Q({data: employee})
+
+       updatedEmployee = {employeeId: "123", employeeName: "Jack", internalNumber: "000"};
+       mongooseServiceUpdatePromise = Q({data: updatedEmployee})
+
+       mongooseServiceDeletePromise = Q({data: "success"})
+
+       mockMongooseService = { findUser: sinon.stub().returns(mongooseServicePromise), updateUser: sinon.stub().returns(mongooseServiceUpdatePromise), deleteUser: sinon.stub().returns(mongooseServiceDeletePromise) };
 
        module('adjuvant', function($provide) {
            $provide.value('mongooseService', mockMongooseService);
@@ -14,7 +21,6 @@ describe('findUser', function() {
     beforeEach(module('partials/findUser.html'));
 
     beforeEach(inject(function($rootScope, $compile) {
-
         element = angular.element('<div find-user></div>');
         scope = $rootScope;
         $compile(element)($rootScope);
@@ -23,13 +29,20 @@ describe('findUser', function() {
 
     it('should reset defaults', function() {
         var isolatedScope = element.isolateScope()
-        expect(isolatedScope.showSuccessAlert).to.be.eql(false)
         expect(isolatedScope.employeeId).to.be.eql('')
+        expect(isolatedScope.employeeName).to.be.eql('')
+        expect(isolatedScope.result).to.be.eql('')
+        expect(isolatedScope.showFindErrorAlert).to.be.eql(false)
+        expect(isolatedScope.shouldDisplayResult).to.be.eql(true)
+        expect(isolatedScope.showDeleteSuccessAlert).to.be.eql(false)
+        expect(isolatedScope.showDeleteErrorAlert).to.be.eql(false)
+        expect(isolatedScope.showUpdateSuccessAlert).to.be.eql(false)
+        expect(isolatedScope.showUpdateErrorAlert).to.be.eql(false)
     })
 
     it('should show form', function() {
-        var labels = element.find('label')
-        expect(labels.text()).to.contain("Employee ID")
+        var enterEmpIDLabel = element.find('label')
+        expect(enterEmpIDLabel.text()).to.contain("Employee ID")
     })
 
     it('Should find a user on submit', function() {
@@ -43,11 +56,12 @@ describe('findUser', function() {
         return mongooseServicePromise.then(function() {
             expect(mockMongooseService.findUser).to.be.calledOnce;
             expect(mockMongooseService.findUser).to.be.calledWith(expectedUser);
-            expect(isolatedScope.result).to.be.eql(["dummy"]);
+            expect(isolatedScope.result).to.be.eql(employee);
+            expect(isolatedScope.employeeName).to.be.eql(employee.employeeName);
         })
     })
 
-    it('Should set showErrorAlert flag to true when service fails', function() {
+    it('Should set appropriate flags when find service fails', function() {
         var expectedUser = { empId: "16305" }
         mockMongooseService.findUser = sinon.stub().returns(Q.reject());
 
@@ -57,7 +71,8 @@ describe('findUser', function() {
         element.find('button')[0].click();
 
         return mongooseServicePromise.then(function() {
-            expect(isolatedScope.showErrorAlert).to.be.eql(true)
+            expect(isolatedScope.showFindErrorAlert).to.be.eql(true)
+            expect(isolatedScope.shouldDisplayResult).to.be.eql(false)
         })
     })
 })
