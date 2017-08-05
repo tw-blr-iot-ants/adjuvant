e8905c528c6f3837477ef082cec2573333c371e7
vikashupadhyay <vikasku@thoughtworks.com>
2017-08-05 18:39:11 +0530

    #0[Vikas] Refactored encryption

--- app/server.js
+++ app/server.js
@@ -30,19 +30,13 @@ app.use(session({
 }));
 
 app.use(function (req, res, next) {
-    var decodedAuth = "";
     if (req.headers.authorization) {
         var decipher = crypto.createDecipher('aes-128-ecb', encryption_key);
-        var chunks;
-
-        chunks = []
-        chunks.push(decipher.update(new Buffer(req.headers.authorization, "base64").toString("binary")));
-        chunks.push(decipher.final('binary'));
-        decodedAuth = chunks.join("");
-        decodedAuth = new Buffer(decodedAuth, "binary").toString("utf-8");
+        decipher.update(new Buffer(req.headers.authorization, "base64").toString("binary"), 'binary', 'utf8');
+        decipher.final();
     }
 
-    if (req.session.password != undefined || "admin:123abc123" == "admin:123abc123" || req.url == '/api/login') {
+    if (req.session.password !== undefined || "admin:123abc123" === "admin:123abc123" || req.url === '/api/login') {
         return next();
     } else {
         res.status(401).send("User is not logged in");
