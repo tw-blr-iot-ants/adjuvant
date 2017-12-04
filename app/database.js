8be02a27fbcdbe92901ee98ccc0c8f6166faa46f
vikashupadhyay <vikasku@thoughtworks.com>
2017-12-04 21:30:41 +0530

    <Vikas> Change log messages

--- app/database.js
+++ app/database.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose');
 var databaseConfig = require('./config/database');
 
 var db = mongoose.connection;
+console.log(databaseConfig.url);
 mongoose.connect(databaseConfig.url);
 
 db.on('error', console.error);
 db.once('open', function() {
-  console.log("Mongo connection successful " + databaseConfig.url)
+  console.log("Mongo connection successful " + databaseConfig.url);
 });
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('./app/database.js');
 
 console.log("********************************************");
-console.log("Mongo url", process.env.MONGODB_URL);
+console.log("Mongo db url", process.env.MONGODB_URL);
 console.log("********************************************");
 
 console.log("********************************************");
-console.log("Mongo url", process.env.MONGODB_TEST_URL);
+console.log("Mongo db test url", process.env.MONGODB_TEST_URL);
 console.log("********************************************");
 
 console.log("********************************************");
-console.log("Mongo url", process.env.MONGO_SESSION_URL);
+console.log("Mongo db session url", process.env.MONGO_SESSION_URL);
 console.log("********************************************");
 console.log("******************");
 
